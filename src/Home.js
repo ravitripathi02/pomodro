e3478f608bf6d4054b57cfdef66a54a4816400d5
ravitripathi02 <91239605+ravitripathi02@users.noreply.github.com>
2023-08-28 11:31:17 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
-import { signOut } from "firebase/auth";
+import { signOut, getAuth, onAuthStateChanged } from "firebase/auth";
+import { useState } from "react";
 import React from "react";
 import "./App.css";
 import { database } from "./FirebaseConfig";
@@ -6,19 +7,34 @@ import { useNavigate } from "react-router-dom";
 import Timer from "./Timer";
 const Home = () => {
   const history = useNavigate();
-
+  const [user, setUser] = useState(null);
+  const auth = getAuth();
+  const unsubscribe = onAuthStateChanged(auth, (user) => {
+    setUser(user);
+  });
+  console.log(user);
+  console.log(unsubscribe);
   const handleClick = () => {
     signOut(database).then((val) => {
       console.log(val, "val");
       history("/");
     });
   };
+  const signUp = () => {
+    history("/");
+  };
   return (
     <div className="App1">
       <Timer />
-      <button onClick={handleClick} className="signout">
-        SignOut
-      </button>
+      {user !== null ? (
+        <button onClick={handleClick} className="signout">
+          SignOut
+        </button>
+      ) : (
+        <button onClick={signUp} className="signout">
+          SignUp
+        </button>
+      )}
     </div>
   );
 };
